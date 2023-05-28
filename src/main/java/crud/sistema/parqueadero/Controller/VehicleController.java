b6c1ce880c4d6cd58b6458a2b05483e080225b84
NelsonDavidGutierrez <nelsondavidgutierrezmedina89@gmail.com>
2023-05-28 11:33:16 -0500

    Implementamos en el controlador para mostrar los usuarios que tienen asignado una id, y tambien se puede eliminar, editar y crear

--- src/main/java/crud/sistema/parqueadero/Controller/VehicleController.java
+++ src/main/java/crud/sistema/parqueadero/Controller/VehicleController.java
@@ -1,5 +1,7 @@
 package crud.sistema.parqueadero.Controller;
+import crud.sistema.parqueadero.Model.Usuario;
 import crud.sistema.parqueadero.Model.Vehicle;
+import crud.sistema.parqueadero.Service.UsuarioService;
 import crud.sistema.parqueadero.Service.VehicleService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -16,49 +19,70 @@ public class VehicleController {
     private final Logger LOGGER = LoggerFactory.getLogger(VehicleController.class);
 
     @Autowired
+    private UsuarioService usuarioService;
+
+    @Autowired
     private VehicleService vehicleService;
 
     @GetMapping("")
     public String show(Model model){
+        model.addAttribute("usuarios", usuarioService.findAllUsuario());
         model.addAttribute("vehiculos", vehicleService.findAll());
-        return "vehiculo/vehiculoDetalle";
+        return "administrador/listaVehiculo";
     }
     @GetMapping("/create")
-    public  String create() { return  "vehiculo/agregarVehiculo";}
+    public  String create(Model model) {
+        model.addAttribute("usuarios", new Usuario());
+        model.addAttribute("vehiculos", vehicleService.findAll());
+        return  "vehiculo/agregarVehiculo";
+    }
 
     @PostMapping("/save")
-    public String save(Vehicle vehicle) {
-        Vehicle o = new Vehicle(1, "", "", "");
-        vehicle.setNombreUsuario(String.valueOf(o));
+    public String save(Usuario usuario, Vehicle vehicle, Model model){
         vehicleService.guardar(vehicle);
+        usuarioService.guardarUsuario(usuario);
+        usuario.setVehicle(Optional.ofNullable(vehicle));
+
+        List<Usuario> usuariosConVehiculos = usuarioService.findAllUsuario();
+
+        model.addAttribute("usuarios", usuariosConVehiculos);
         return "redirect:/vehicles";
     }
 
     @GetMapping("/edit/{id}")
     public String edit(@PathVariable Integer id, Model model) {
-        Optional<Vehicle> optionalVehicle = vehicleService.get(id);
-        optionalVehicle.ifPresentOrElse(
-                vehicle -> {
-                    LOGGER.info("Vehiculo buscado: {}", vehicle);
-                    model.addAttribute("vehiculo", vehicle);
-                },
-                () -> {
-                    model.addAttribute("errorMessage", "El vehículo con el ID " + id + " no se encontró.");
-                }
-        );
-        return "vehiculo/editarVehiculo";
+        Optional<Usuario> optionalUsuario = usuarioService.getUsuario(id);
+        if (optionalUsuario.isPresent()) {
+            Usuario usuario = optionalUsuario.get();
+            model.addAttribute("usuario", usuario);
+            model.addAttribute("vehiculos", vehicleService.findAll());
+            return "vehiculo/editarVehiculo";
+        } else {
+            model.addAttribute("errorMessage", "El usuario con el ID " + id + " no se encontró.");
+            return "redirect:/vehicles";
+        }
     }
 
 
     @PostMapping("/update")
-    public String update(Vehicle vehicle){
+    public String update(Vehicle vehicle, Usuario usuario){
         vehicleService.update(vehicle);
+        usuarioService.updateUsuario(usuario);
         return "redirect:/vehicles";
     }
 
     @GetMapping("/delete/{id}")
     public String delete(@PathVariable Integer id, Model model){
-        vehicleService.delete(id);
+        Vehicle vehicle = (Vehicle) vehicleService.findAll();
+        if (vehicle != null) {
+            Usuario usuario = usuarioService.findByVehicleId(id);
+            if (usuario != null) {
+                usuario.setVehicle(null);
+                usuarioService.guardarUsuario(usuario);
+            }
+            vehicleService.delete(vehicle.getId());
+        }
         return "redirect:/vehicles";
+
     }
 }
